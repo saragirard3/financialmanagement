bbea782b9f876455857b88e89fd867662d4b4fde
saragirard <sarag.bsa@gmail.com>
2023-08-17 06:59:53 -0400

    first commit

---
+++ src/test/java/com/saragirard/financialmanagement/FinancialManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saragirard.financialmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
